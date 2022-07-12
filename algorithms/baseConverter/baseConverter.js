d2560697e14a4eff02a9af0a4a919ee0b95b7895
hanyelC <hanyel.chamon@gmail.com>
2022-07-12 10:27:36 -0300

    implementing base converter

---
+++ algorithms/baseConverter/baseConverter.js
@@ -0,0 +1,26 @@
+const Stack = require("../../data-structures/stack/stack")
+
+function baseConverter(decNumber, base) {
+  const remStack = new Stack()
+  const digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let number = decNumber
+  let rem
+  let baseString = ''
+  if (!(base >= 2 && base <= 36)) {
+    return ''
+  }
+
+  while (number > 0) {
+    rem = Math.floor(number % base)
+    remStack.push(rem)
+    number = Math.floor(number / base)
+  }
+
+  while (!remStack.isEmpty()) {
+    baseString += digits[remStack.pop()]
+  }
+
+  return baseString
+}
+
+module.exports = baseConverter
